edbeed6243573615e08f2f2aac1753e8ec4659f9
Trần Thanh Điền <41979076+tranthanhdien@users.noreply.github.com>
2019-05-15 17:43:09 +0700

    Update ControllerLogin

--- CNPM/src/controller/LoginController.java
+++ CNPM/src/controller/LoginController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,7 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model_LoginAccount.LoginDAO;
+import model.Login;
+import model.LoginAccount;
 
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
@@ -24,23 +28,24 @@ public class LoginController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 5. Lấy thông tin từ Form 
+		
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-
+		// 5. Khởi tạo hình thức đăng nhập
+		LoginAccount account = new LoginAccount();
+		// 6. Lấy thông tin từ Form 
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("pass");
-		// 5. Kiểm tra thông tin trong database
-		LoginAccount account = new LoginAccount();
+		// 7. Kiểm tra thông tin trong database
 		if (account.login(userName, password)) {
-			// 6.1 Nếu thông tin hợp lệ thì chuyển qua trang login thành công
+			// 8. Nếu thông tin hợp lệ thì chuyển qua trang login thành công
 			PrintWriter pw = response.getWriter();
 			pw.println("Logged in successfully</h2>");
 			pw.println("Username: " + userName);
 			pw.println("Password: " + password);
 		} else {
-			// 7.1 Nếu thông tin không hợp lệ thì quay lại trag đăng nhập và cho phép nhập lại
+			// 9. Nếu thông tin không hợp lệ thì quay lại trag đăng nhập và cho phép nhập lại
 			RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
 			request.setAttribute("message", "Login failed, type again!");
 			rd.forward(request, response);
