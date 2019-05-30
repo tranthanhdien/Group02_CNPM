556d33595fa7831fa4b78ac7fc5f3942f6afe63b
Dương Ngọc Quí <46017134+DNQ98@users.noreply.github.com>
2019-05-30 10:28:09 +0700

    Update RestFB.java

--- CNPM/src/model_LoginFacebook/RestFB.java
+++ CNPM/src/model_LoginFacebook/RestFB.java
@@ -13,19 +13,21 @@ import com.restfb.types.User;
 import common.Constants;
 
 public class RestFB {
-
+	// 7. Lấy thông tin: APP_ID, APP_SECRET,REDIRECT_URL, LINK_GET_TOKEN
 	public static String getToken(final String code) throws ClientProtocolException, IOException {
 		String link = String.format(Constants.FACEBOOK_LINK_GET_TOKEN, Constants.FACEBOOK_APP_ID,
 				Constants.FACEBOOK_APP_SECRET, Constants.FACEBOOK_REDIRECT_URL, code);
 		String response = Request.Get(link).execute().returnContent().asString();
 		JsonObject jobj = new Gson().fromJson(response, JsonObject.class);
 		String accessToken = jobj.get("access_token").toString().replaceAll("\"", "");
+		// 9. Trả về AccessToken
 		return accessToken;
 	}
 
 	public static User getUserInfo(String accessToken) {
 		FacebookClient facebookClient = new DefaultFacebookClient(accessToken, Constants.FACEBOOK_APP_SECRET,
 				Version.LATEST);
+		// 11.Trả về User facebookClient.fetchObject("me", User.class)
 		return facebookClient.fetchObject("me", User.class);
 	}
 }
