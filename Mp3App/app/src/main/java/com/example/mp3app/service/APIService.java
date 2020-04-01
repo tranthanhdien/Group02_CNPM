57dc7d861c27a5b15902232a7e612a01777247c6
Trần Thanh Điền <41979076+tranthanhdien@users.noreply.github.com>
2020-04-01 08:43:01 +0700

    Test
    Reomve MP3 Source Code

--- Mp3App/app/src/main/java/com/example/mp3app/service/APIService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mp3app.service;
-
-public class APIService {
-    public static String base_url = "https://ttdien.000webhostapp.com/Server/";
-
-    public static DataService getService() {
-        return APIRetrofitClient.getClient(base_url).create(DataService.class);
-    }
-}
